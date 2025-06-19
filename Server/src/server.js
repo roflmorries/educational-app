2ead7df51337593759d8ef438bf0bfce9679729a
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-19 22:58:06 +0300

    connected db to app

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,6 +1,9 @@
+import { connectDB } from "./config/db.js";
 import app from "./main.js";
 
 const PORT = process.env.PORT || 3000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
