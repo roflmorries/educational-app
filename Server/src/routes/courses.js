06f9f6744b0bec05e6eb58c570e80e7b0c70df98
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-25 14:29:03 +0300

    added insert/update/replace/delete many

--- Server/src/routes/courses.js
+++ Server/src/routes/courses.js
@@ -53,6 +53,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.post('/many', async (req, res) => {
+  try {
+    const db = getDB();
+    const courses = req.body;
+
+    const result = await db.collection(COLLECTION).insertMany(courses);
+    const inserted = courses.map((course, i) => ({
+      ...course,
+      _id: result.insertedIds[i]
+    }));
+    res.status(201).send(inserted);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
 router.put('/assign-student/:courseId', async (req, res) => {
   try {
     const { studentId } = req.body;
@@ -100,6 +117,34 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.put('/many', async (req, res) => {
+  try {
+    const db = getDB();
+    const { filter, update } = req.body;
+
+    if (!filter || !update) return res.status(400).json({ error: 'Missed filter and update' });
+    
+    const result = await db.collection(COLLECTION).updateMany(filter, { $set: update});
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put('/replace/:id', async (req, res) => {
+  try {
+    const db = getDB();
+    const replacement = req.body;
+    const result = await db.collection(COLLECTION).replaceOne({ _id: new ObjectId(req.params.id) }, replacement);
+    if (result.matchedCount === 0) return res.status(404).json({ error: 'Course doesnt exists' });
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+})
+
 router.delete("/:id", async (req, res) => {
   try {
     const db = getDB();
@@ -112,12 +157,13 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
-router.delete('/', async (req, res) => {
+router.delete('/many', async (req, res) => {
   try {
     const db = getDB();
-    const deletedCounter = req.body;
-    const result = db.collection(COLLECTION).deleteMany(deletedCounter);
-    res.json({ deletedCount: result.deletedCount  })
+    const deleteCounter = req.body;
+    if (!deleteCounter) return res.status(400).json({ error: 'Missed deleteCounter' })
+    const result = db.collection(COLLECTION).deleteMany(deleteCounter);
+    res.json({ deleteCount: result.deleteCounter  })
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' })
